0cb8898b1e2da9deb27f801cf913ee9ed758e869
bim121 <vitisik54@gmail.com>
2023-08-14 23:49:56 +0300

    init

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Footer from "./Footer";
+import Meta from "./Meta"
+import Navbar from "./Navbar";
+
+
+const Layout = ({children}) => {
+    return(
+        <>
+            <Meta />
+            <Navbar />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import Hero from "../components/Hero";
+import PopularMovie from "../components/PopularMovie";
+import { server } from "../config"
+
+
+export default function Home({movies}){
+    console.log(movies)
+    return(
+        <div className="bg-gray-700">
+            <Hero />
+            <PopularMovie movies={movies.results} />
+        </div>
+    )
+}
+
+export async function getStaticProps(){
+    const res = await axios(`${server}/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`)
+    const movies = res.data;
+
+    return{
+        props: {movies}
+    }
+}
